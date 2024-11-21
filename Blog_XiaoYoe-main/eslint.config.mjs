45742d0c51161d618d729e34a3474a5652711be1
小约 <132027228+Xiaoyoe@users.noreply.github.com>
2024-11-21 13:34:11 +0800

    Delete Blog_XiaoYoe-main directory 手动回滚（我服了

--- Blog_XiaoYoe-main/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import antfu from '@antfu/eslint-config'
-
-export default antfu({
-  formatters: true,
-}).override('antfu/javascript/rules', {
-  rules: {
-    'no-console': 'off',
-  },
-})
--- Blog_XiaoYoe-main/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './mock/index'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
-// createApp(App).mount('#app')
